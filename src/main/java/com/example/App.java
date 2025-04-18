46ec24ee763cbdbf6f5a69c0f31bfb2b33697a6a
vam1207 <vam.1207@gmail.com>
2025-04-18 19:29:10 +0530

    Initial commit: Maven Java project setup

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven Java Project!");
+    }
+
+    public String getGreeting() {
+        return "Welcome to Maven!";
+    }
+}
